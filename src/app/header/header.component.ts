1abedf068ee1a595eaf521abd57875351f1b1e8d
Shanaka <shanakamunasingheplay@gmail.com>
2019-01-14 07:49:45 +0530

    take out static components in the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,8 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent implements OnInit { 
+  header = {
+    heading:'MedHelp',
+    headingtext:'Namari is a free landing page template you can use for your projects. It is free to use for your personal and commercial projects, enjoy!',
+    buttontext: 'do some action!',
+    buttonlink:'\about', 
+  }
   constructor() { }
 
   ngOnInit() {
